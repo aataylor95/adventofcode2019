4ad740b3b863cf49b845f1721f4b9abd884db588
Adam Taylor <adam@carsnip.com>
2019-12-01 19:16:26 +0000

    task2

--- src/main/groovy/com/github/aataylor95/day1/FuelCalculator.groovy
+++ src/main/groovy/com/github/aataylor95/day1/FuelCalculator.groovy
@@ -5,16 +5,39 @@ import groovy.json.JsonSlurper
 class FuelCalculator {
 
   static void main(String[] args) {
-    println new FuelCalculator().calculateTotalFuel()
+    def calculator = new FuelCalculator()
+
+    println "Task 1: ${calculator.task1()}"
+    println "Task 2: ${calculator.task2()}"
   }
 
-  Integer calculateTotalFuel() {
-    List<Integer> moduleMasses = new JsonSlurper().parse(this.class.getResourceAsStream('/day1/input.json'))
+  Integer task1() {
+    moduleMasses.sum { calculateModuleFuel(it) } as Integer
+  }
 
-    return (Integer)moduleMasses.sum { calculateModuleFuel(it) }
+  Integer task2() {
+    (Integer)moduleMasses.sum {
+      addExtraFuel calculateModuleFuel(it)
+    }
+  }
+
+  Integer addExtraFuel(Integer moduleFuel) {
+    Integer extraFuel = calculateModuleFuel(moduleFuel)
+
+    while (extraFuel > 0) {
+      moduleFuel += extraFuel
+
+      extraFuel = calculateModuleFuel(extraFuel)
+    }
+
+    return moduleFuel
   }
 
   Integer calculateModuleFuel(Integer moduleMass) {
     (moduleMass.toBigDecimal() / 3).trunc() - 2
+  }
+
+  List<Integer> getModuleMasses() {
+    (List<Integer>)new JsonSlurper().parse(this.class.getResourceAsStream('/day1/input.json'))
   }
 }
